fa0a19fd8a86c5e68be2d36fdc6f7ddd14ad9c5f
David Rhoads <davidrhoads@mailstack.com>
2026-01-10 13:45:49 -0500

    Change sandbox server hostname

--- hosts/sandbox/default.nix
+++ hosts/sandbox/default.nix
@@ -3,12 +3,12 @@
 {
   imports = [ ../../modules/media-server.nix ];
   
-  networking.hostName = "sandbox-media";       # per-host can override if desired
+  networking.hostName = "T29769";       # per-host can override if desired
   time.timeZone = "America/New_York";
 
   services.mediaServer = {
     enable = true;
-    domainBase = "kirk-media.ccistack.com"; # site-specific base domain
+    domainBase = "sandbox-media.ccistack.com"; # site-specific base domain
     tlsMode = "none";                        # or "internal" if you want internal CA
 
     paths = {
