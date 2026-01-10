44a6e1b176cfe4c10fc16ba9643fabad4baa570c
David Rhoads <davidrhoads@mailstack.com>
2026-01-10 10:48:00 -0500

    Add comin module to flake

--- flake.nix
+++ flake.nix
@@ -3,10 +3,14 @@
   description = "Nix Media Server: reusable multi-site media server (Jellyfin + Audiobookshelf)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    comin = {
+          url = "github:nlewo/comin";
+          inputs.nixpkgs.follows = "nixpkgs";
+        };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, comin }:
   let
     system = "x86_64-linux";
     hostNames = builtins.attrNames (builtins.readDir ./hosts);
@@ -20,6 +24,17 @@
             ./modules/common.nix
             ./hosts/${host}/default.nix
             ./hosts/${host}/hardware.nix
+            comin.nixosModules.comin
+            (
+              services.comin = {
+                enable = true;
+                remotes = [(
+                  name = "origin";
+                  url = "https://github.com/kd2flz/nix-media-server";
+                  branches.main.name = "main";
+                )]
+              }
+            )
           ];
         };
       }) hostNames);
