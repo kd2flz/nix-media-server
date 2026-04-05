f989615f53bec6ab54d71828c4ad2b4dfe972ced
David Rhoads <davidrhoads@mailstack.com>
2026-04-05 13:30:14 -0400

    Add comparison operators #41

--- modules/monitoring.nix
+++ modules/monitoring.nix
@@ -241,7 +241,7 @@ in
                       type = "prometheus";
                       uid = "PBFA97CFB590B2093";
                     };
-                    expr = "(1 - (node_filesystem_avail_bytes{mountpoint=\"/\"} / node_filesystem_size_bytes{mountpoint=\"/\"})) * 100 > 90";
+                    expr = "(1 - (node_filesystem_avail_bytes / node_filesystem_size_bytes)) * 100 > 90";
                     intervalMs = 1000;
                     maxDataPoints = 43200;
                     refId = "A";
