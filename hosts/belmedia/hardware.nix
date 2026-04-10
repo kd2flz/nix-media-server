137964aa961c3d54ad51d1a5e2ffb5bea0ffbc8c
David Rhoads <davidrhoads@mailstack.com>
2026-04-10 14:30:30 -0400

    Fix belmedia boot loader config Refs #41

--- hosts/belmedia/hardware.nix
+++ hosts/belmedia/hardware.nix
@@ -37,4 +37,6 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  boot.loader.grub.devices = [ "/dev/sda" ];
 }
