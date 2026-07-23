9767b02b07bbc70bc9cbf8d1418c26af70f56d58
David Rhoads <davidrhoads@mailstack.com>
2026-07-23 07:57:49 -0400

    Force override of older NVIDIA driver

--- hosts/P22725/hardware.nix
+++ hosts/P22725/hardware.nix
@@ -48,5 +48,4 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  hardware.nvidia.package = pkgs.nvidiaPackages.legacy_580;
 }
